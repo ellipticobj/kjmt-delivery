6d52ec9b11e5a7c5f0aaa6c20b713a6be9786382
four hundred kilovolts of electricity suddenly passes through a mysterious elliptic object floating in the sky <ellipticobj@gmail.com>
2024-08-15 23:31:32 +0800

    WORKING BUILD cleaned up and fixed code, this one works

--- main.py
+++ main.py
@@ -6,14 +6,10 @@ import cogs.manager as m
 print("init sequence beginning")
 client = int(datafun.fetchdatabyid("channels.json", "client"))
 manager = int(datafun.fetchdatabyid("channels.json", "manager"))
-log = int(datafun.fetchdatabyid("channels.json", "log"))
 settings = int(datafun.fetchdatabyid("channels.json", "dev"))
-guild = int(datafun.fetchdatabyid("channels.json", "guild"))
-log = int(datafun.fetchdatabyid("channels.json", "log"))
 
 # starting bot
-client = discord.Bot()
-log.message.send("bot starting...")
+bot = discord.Bot()
 
 # loading cogs
 cogs_list = [
@@ -23,19 +19,28 @@ cogs_list = [
 
 for cog in cogs_list:
     try:
-        log.message.send("attempting to load cogs.{cog}...")
-        client.load_extension(f'cogs.{cog}')
-        log.message.send("loaded cogs.{cog}")
+        print(f"attempting to load cogs.{cog}...")
+        bot.load_extension(f'cogs.{cog}')
+        print(f"loaded cogs.{cog}")
     except Exception as e:
-        log.message.send("ERROR??!?!?!?!?!??? output:\n{e}\nend of output... aw :(")
-log.message.send("loaded all cogs")
+        print(f"ERROR??!?!?!?!?!??? output:\n{e}\nend of output... aw :(")
+print("loaded all cogs")
 
 async def comingsoon(ctx):
     await ctx.respond("coming soon!")
     
-@client.event
+@bot.event
 async def on_ready():
-    log.message.send("init sequence successful!! :3")
+    print("init sequence complete!! :3")
+    
+
+@bot.slash_command(name="test")
+async def test(ctx):
+    await ctx.respond("bot is working!")
+
+@bot.slash_command(name="order")
+async def order(ctx):
+    comingsoon()
     
 load = discord.SlashCommandGroup("load", "admin command for loading cogs")
 unload = load.create_subgroup("unload", "admin command for unloading cogs")
@@ -43,48 +48,38 @@ unload = load.create_subgroup("unload", "admin command for unloading cogs")
 @load.command()
 async def manager(ctx):
     try:
-        log.message.send("attempting to load cogs.manager...")
-        client.load_extension(f'cogs.manager')
-        log.message.send("loaded cogs.manager")
+        print("attempting to load cogs.manager...")
+        bot.load_extension(f'cogs.manager')
+        print("loaded cogs.manager")
     except Exception as e:
-        log.message.send("ERROR??!?!?!?!?!??? output:\n{e}\nend of output... aw :(")
+        print(f"ERROR??!?!?!?!?!??? output:\n{e}\nend of output... aw :(")
     
 @load.command()
 async def client(ctx):
     try:
-        log.message.send("attempting to load cogs.client...")
-        client.load_extension(f'cogs.client')
-        log.message.send("loaded cogs.client")
+        ctx.response("attempting to load cogs.client...")
+        bot.load_extension(f'cogs.client')
+        ctx.response("loaded cogs.client")
     except Exception as e:
-        log.message.send("ERROR??!?!?!?!?!??? output:\n{e}\nend of output... aw :(")
+        print(f"ERROR??!?!?!?!?!??? output:\n{e}\nend of output... aw :(")
     
 @unload.command()
 async def manager(ctx):
     try:
-        log.message.send("attempting to unload cogs.manager...")
-        client.unload_extension(f'cogs.manager')
-        log.message.send("unloaded cogs.manager")
+        ctx.response("attempting to unload cogs.manager...")
+        bot.unload_extension(f'cogs.manager')
+        ctx.response("unloaded cogs.manager")
     except Exception as e:
-        log.message.send("ERROR??!?!?!?!?!??? output:\n{e}\nend of output... aw :(")
+        print(f"ERROR??!?!?!?!?!??? output:\n{e}\nend of output... aw :(")
 
 @unload.command()
 async def client(ctx):
     try:
-        log.message.send("attempting to unload cogs.client...")
-        client.unload_extension(f'cogs.client')
-        log.message.send("unloaded cogs.client")
+        ctx.response("attempting to unload cogs.client...")
+        bot.unload_extension(f'cogs.client')
+        ctx.response("unloaded cogs.client")
     except Exception as e:
-        log.message.send("ERROR??!?!?!?!?!??? output:\n{e}\nend of output... aw :(")
-
-@client.slash_command(name="test", guild_id=guild)
-async def test(ctx):
-    await ctx.respond("slash commands working!")
-
-@client.slash_command(name="order", guild_id=guild)
-async def order(ctx):
-    comingsoon()
-
-
+        print(f"ERROR??!?!?!?!?!??? output:\n{e}\nend of output... aw :(")
 
 # starting the bot itself
-client.run(datafun.fetchdatabyid("dat.json", "token"))
+bot.run(datafun.fetchdatabyid("dat.json", "token"))
