7106485fb7526343b24f04ee512b77972ddc0c31
four hundred kilovolts of electricity suddenly passes through a mysterious elliptic object floating in the sky <ellipticobj@gmail.com>
2024-09-17 10:25:46 +0800

    chan

--- cogs/account.py
+++ cogs/account.py
@@ -6,9 +6,22 @@ class Account(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
+    defaultvals = {
+            "username": "Anon",
+            "displayname": "Anon",
+            "deliveryperson": False,
+            "orders": [],
+            "cards": [],
+            "addresses": [],
+            "loyalty": [],
+            "banned": False,
+            "staff": False,
+            "password": "d919a100ce6b45524d415d52d088d5817587c6dd8c3691b03b8063c44d043523"
+        }
+        
     account = discord.SlashCommandGroup("account", "commands related to accounts")
     
-    @account.command(name="create")
+    @account.command(name="create", username="Anon", displayname="Anon")
     async def createacc(self, ctx):
         await ctx.response(f"Account creation in progress...")
         
